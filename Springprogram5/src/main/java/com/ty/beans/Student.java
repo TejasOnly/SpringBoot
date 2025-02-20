27a6160d0fa4296b1a43119cd0a38f8f3349939d
Master <Master@DESKTOP-A9RLOOJ>
2025-02-20 17:08:37 +0530

    Initial Commit.

---
+++ Springprogram5/src/main/java/com/ty/beans/Student.java
@@ -0,0 +1,48 @@
+package com.ty.beans;
+
+public class Student {
+	
+	private String Name;
+	private int Rollno;
+	private Address address;
+	
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getRollno() {
+		return Rollno;
+	}
+
+	public void setRollno(int rollno) {
+		Rollno = rollno;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	
+	
+	
+	public void Display()
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("Rollno :"+Rollno);
+		System.out.println("Address :"+address);
+	}
+	
+	
+	
+	
+	
+
+}
